f2dfe7c604508c76ddb174f79f55a5997bea7cba
jayucrascz <119620440+jayucrascz@users.noreply.github.com>
2023-02-08 10:22:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const publicPath = path.resolve(__dirname,'public');
 app.use(express.static(publicPath));
 
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 module.exports.io = require('socket.io')(server);
 
 require('./sockets/socket');
